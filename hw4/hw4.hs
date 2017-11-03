69d3b25b07a9e802d1f756af44bfcdaa8325eb34
sahabi <sahabi@gmail.com>
2017-11-02 19:56:51 -0500

    solved 4.3

--- hw4/hw4.hs
+++ hw4/hw4.hs
@@ -1,9 +1,11 @@
+-- exercise 1
 fun1 :: [Int] -> Int
 fun1 = product . map (subtract 2) . filter even 
 
 fun2 :: Int -> Int
 fun2 n = sum lst where lst = takeWhile (/= 1) $ iterate (\x -> if even x then x `div` 2 else 3*x+1) n
 
+-- exercise 2
 data Tree a = Leaf
             | Node Int (Tree a) a (Tree a)
     deriving (Show, Eq, Ord)
@@ -24,5 +26,13 @@ treeLevel :: Tree a -> Int
 treeLevel Leaf = 0
 treeLevel (Node n _ _ _) = n 
 
+-- exercise 3
+xor :: [Bool] -> Bool
+xor = odd . length . filter ((&&) True)
 
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr f' [] where f' = (:) . f
+
+myFoldl :: (a -> b -> a) -> a -> [b] -> a
+myFoldl f base = foldr (flip f) base . reverse
 
