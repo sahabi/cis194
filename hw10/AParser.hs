86044681cabec7ba3d4d1b862ab7e76e7516f100
sahabi <sahabi@gmail.com>
2018-01-16 19:57:49 -0600

    added hw10

---
+++ hw10/AParser.hs
@@ -0,0 +1,98 @@
+{- CIS 194 HW 10
+   due Monday, 1 April
+-}
+
+module AParser where
+
+import           Control.Applicative
+
+import           Data.Char
+
+-- A parser for a value of type a is a function which takes a String
+-- represnting the input to be parsed, and succeeds or fails; if it
+-- succeeds, it returns the parsed value along with the remainder of
+-- the input.
+newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
+
+-- For example, 'satisfy' takes a predicate on Char, and constructs a
+-- parser which succeeds only if it sees a Char that satisfies the
+-- predicate (which it then returns).  If it encounters a Char that
+-- does not satisfy the predicate (or an empty input), it fails.
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy p = Parser f
+  where
+    f [] = Nothing    -- fail on the empty input
+    f (x:xs)          -- check if x satisfies the predicate
+                        -- if so, return x along with the remainder
+                        -- of the input (that is, xs)
+        | p x       = Just (x, xs)
+        | otherwise = Nothing  -- otherwise, fail
+-- Using satisfy, we can define the parser 'char c' which expects to
+-- see exactly the character c, and fails otherwise.
+char :: Char -> Parser Char
+char c = satisfy (== c)
+
+
+
+{- For example:
+
+*Parser> runParser (satisfy isUpper) "ABC"
+Just ('A',"BC")
+*Parser> runParser (satisfy isUpper) "abc"
+Nothing
+*Parser> runParser (char 'x') "xyz"
+Just ('x',"yz")
+
+-}
+
+-- For convenience, we've also provided a parser for positive
+-- integers.
+posInt :: Parser Integer
+posInt = Parser f
+  where
+    f xs
+      | null ns   = Nothing
+      | otherwise = Just (read ns, rest)
+      where (ns, rest) = span isDigit xs
+
+------------------------------------------------------------
+-- Your code goes below here
+------------------------------------------------------------
+first :: (a -> b) -> (a,c) -> (b,c)
+first f (x,s) = (f x, s)
+
+second :: (b -> c) -> (a,b) -> (a,c)
+second f (x,s) = (x, f s)
+
+
+instance Functor Parser where
+    fmap f (Parser rp) = Parser ((fmap (first f)) . rp)
+
+instance Applicative Parser where
+    pure a = Parser f where f str = Just (a, str)
+    p1 <*> p2 = Parser f where
+                f str = case runParser p1 str of
+                         Nothing -> Nothing
+                         Just (f1,st) -> (first f1) <$> (runParser p2 st)
+
+-- exercise 3
+abParser :: Parser (Char, Char)
+abParser = (,) <$> char 'a' <*> char 'b'
+
+abParser_ :: Parser ()
+abParser_ = (\x y -> ()) <$> char 'a' <*> char 'b'
+
+intPair :: Parser [Integer]
+intPair = (\x y -> [x,y]) <$> posInt <* char ' ' <*> posInt
+
+-- exercise 4
+instance Alternative Parser where
+  empty = Parser $ const empty
+  p1 <|> p2 = Parser $ \s -> runParser p1 s <|> runParser p2 s
+
+-- exercise 5
+ignore :: Parser a -> Parser ()
+ignore = (const () <$>)
+
+intOrUppercase :: Parser Char
+intOrUppercase = posInt <|> (satisfy isUpper)
